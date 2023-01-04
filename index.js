7907ab8809cdb69c39f15bd8199ae2988a93a1b0
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-04 13:32:43 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-ReactDOM.render(
-  <h1>Fitness for everyone!</h1>,
-  document.getElementById("root")
+const page = (
+    <div>
+        <h1>Fitness for everyone!</h1>
+        <p>This is something new</p>
+    </div>
 );
 
 ReactDOM.render(
-  <ul>
-    <li>Gym</li>
-    <li>Protein</li>
-  </ul>,
-  document.getElementById("hood")
+  page,
+  document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <ul>
+//     <li>Gym</li>
+//     <li>Protein</li>
+//   </ul>,
+//   document.getElementById("hood") 
+// );
+
+// function MainContent() {
+//     return (
+//         <h1>Navbar testing!</h1>
+//     )
+// };
+
+// ReactDOM.render(
+//     <div>
+//         <Navbar />
+//         <MainContent />
+//     </div>,
+//     document.getElementById("root")
+// );
