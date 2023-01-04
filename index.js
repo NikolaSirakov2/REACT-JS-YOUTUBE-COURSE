375db6b1fc1598889b1c1239eb579126343d4511
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-04 12:22:26 +0200

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+ReactDOM.render(
+  <h1>Fitness for everyone!</h1>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <ul>
+    <li>Gym</li>
+    <li>Protein</li>
+  </ul>,
+  document.getElementById("hood")
+);
