870860f0bf3735e4660a92f06aeb6b163ad4153e
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-14 14:57:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-ReactDOM.render(<h1>FITNESS SAIT</h1>, document.getElementById("root"));
+// ReactDOM.render(<h1>FITNESS SAIT</h1>, document.getElementById("root"));
+
+function MainContent() {
+    return <h1>Hello</h1>
+} 
+
+ReactDOM.render(
+    <div>
+    <MainContent />
+    </div>,
+    document.getElementById("root")
+    
+)
 
 ReactDOM.render(
   <nav>
@@ -9,3 +21,8 @@ ReactDOM.render(
     </ul>
   </nav>,
   document.getElementById("hood"));
+
+const hola = document.createElement("h1");
+hola.textContent = "THIS IS A TEST";
+hola.className = "header";
+document.getElementById("bar").append(hola);
