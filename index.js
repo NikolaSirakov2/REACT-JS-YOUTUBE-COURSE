a06223123c2a51ab49772fd1af4ee9d8fc7845ed
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-14 15:29:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 // ReactDOM.render(<h1>FITNESS SAIT</h1>, document.getElementById("root"));
 
 function MainContent() {
-    return <h1>Hello</h1>
-} 
+  return <h1>FITNESS SAIT</h1>;
+}
+
+function FitnessStuff() {
+  return <h3>FITNESS ITEMS</h3>;
+}
 
 ReactDOM.render(
-    <div>
-    <MainContent />
-    </div>,
-    document.getElementById("root")
-    
-)
+  <MainContent />,
+//   <FitnessStuff />,
+
+  document.getElementById("root")
+);
 
 ReactDOM.render(
+    <FitnessStuff />,
+
+    document.getElementById("bar")
+);
+
+const page = (
   <nav>
     <ul>
       <li>Food</li>
       <li>Suplements</li>
       <li>Clothes</li>
     </ul>
-  </nav>,
-  document.getElementById("hood"));
+  </nav>
+);
 
-const hola = document.createElement("h1");
-hola.textContent = "THIS IS A TEST";
-hola.className = "header";
-document.getElementById("bar").append(hola);
+ReactDOM.render(page, document.getElementById("hood"));
+
+// const hola = document.createElement("h1");
+// hola.textContent = "THIS IS A TEST";
+// hola.className = "header";
+// document.getElementById("bar").append(hola);
