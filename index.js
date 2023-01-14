82c7eb92106b8d41250bb8ebfe143720d4c49870
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-14 14:21:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+const page = (
+    <nav>
+        <h1>Dragon Fitness</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
 
-// const page = (
-//     <nav>
-//         <h1>Dragon Fitness</h1>
-//         <ul>
-//             <li>Pricing</li>
-//             <li>About</li>
-//             <li>Contact</li>
-//         </ul>
-//     </nav>
-// );
+ReactDOM.render(
+  page,
+  document.getElementById("root")
+);
 
-// ReactDOM.render(
-//   page,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <ul>
+    <li>Gym</li>
+    <li>Protein</li>
+  </ul>,
+  document.getElementById("hood") 
+);
 
-// ReactDOM.render(
-//   <ul>
-//     <li>Gym</li>
-//     <li>Protein</li>
-//   </ul>,
-//   document.getElementById("hood") 
-// );
+function MainContent() {
+    return (
+        <h1>Navbar testing!</h1>
+    )
+};
 
-// // function MainContent() {
-// //     return (
-// //         <h1>Navbar testing!</h1>
-// //     )
-// // };
-
-// // ReactDOM.render(
-// //     <div>
-// //         <Navbar />
-// //         <MainContent />
-// //     </div>,
-// //     document.getElementById("root")
-// // );
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+);
