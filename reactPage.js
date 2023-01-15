33e38e31a189454ffc8661163c2ae218b89ac757
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-15 10:38:28 +0200

    component

--- reactPage.js
+++ reactPage.js
@@ -1,21 +1,17 @@
-const head = (
-    <div>
+function Head() {
+    return <div>
         <img src="logo2.png" width="240px" />
         <h1>Fun facts about React</h1>
-    </div>
-)
-
-const list = (
-    <nav>
         <ul>
             <li>Was first released in 2013</li>
             <li>was originally created by Jordan Walke</li>
             <li>Has well over 100K stars on Github</li>
             <li>Is maintained by facebook</li>
             <li>Powers thousands of enterprise apps</li>
-        </ul>
-    </nav>
-)
+        </ul> 
+    </div>
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(head);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Head />);
 ReactDOM.createRoot(document.getElementById("hood")).render(list);
